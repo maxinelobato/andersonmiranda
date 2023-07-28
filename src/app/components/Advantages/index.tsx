bdeae55e57bcb6031c204e309b8625d43f90e7fc
maxinelobato <barretolobato.org@gmail.com>
2023-07-27 23:10:08 -0300

    changes

--- src/app/components/Advantages/index.tsx
+++ src/app/components/Advantages/index.tsx
@@ -146,11 +146,11 @@ export default function Advantages() {
           <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
             <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-screen-2xl md:mb-0">
               <h1 className="max-w-3xl mb-6 font-sans text-3xl font-bold text-white sm:text-5xl md:mx-auto">
-                Você já ouviu falar de{" "}
+                Você já ouviu falar de <br />{" "}
                 <span className="text-orange-500">
-                  {" "}
-                  Mesa Proprietária Americana?
+                  Mesa Proprietária Americana
                 </span>
+                ?
               </h1>
               <p className="text-center font-medium text-xs text-white md:text-lg md:mt-2 md:block">
                 É quando uma corretora americana disponibiliza um capital a um
@@ -170,6 +170,35 @@ export default function Advantages() {
                 </div>
               ))}
             </div>
+
+            <div className="max-w-xl mt-10 mb-10 md:mx-auto sm:text-center lg:max-w-screen-2xl md:mb-0">
+              <p className="text-center font-medium text-xs text-white md:text-lg md:mt-2 md:block">
+                Obs.: Todas as contas passam por 2 fases de avaliação:
+              </p>
+              <p className="text-center font-normal text-xs text-orange-500 md:text-lg md:mt-2 md:block">
+                <span className="font-bold">1ª Fase de Avaliação</span> ={" "}
+                <span className="font-bold">8% da conta</span>, ou seja, em uma
+                conta de
+                <span className="font-bold"> $100k</span> é o equivalente a{" "}
+                <span className="font-bold">$8 mil dólares</span>
+              </p>
+              <p className="text-center font-normal text-xs text-orange-500 md:text-lg md:mt-2 md:block">
+                <span className="font-bold">2ª Fase de Avaliação</span> ={" "}
+                <span className="font-bold">5% da conta</span>, ou seja, em uma
+                conta de
+                <span className="font-bold"> $100k</span> é o equivalente a{" "}
+                <span className="font-bold">$5 mil dólares</span>
+              </p>
+              <p className="text-center font-normal text-xs text-orange-500 md:text-lg md:mt-2 md:block">
+                <span className="font-bold">Conta Real</span> ={" "}
+                <span className="font-bold">13% da conta</span>, ou seja, em uma
+                conta de
+                <span className="font-bold"> $100k</span> é o equivalente a{" "}
+                <span className="font-bold">$13 mil dólares</span> irá começar
+                novamente para o recebimento do saque em seu nome
+              </p>
+            </div>
+
             <PriceCards />
           </div>
         </section>
