148c33207bdaab97fb6f151e9202e94cb91b52ce
maxinelobato <maxinelobato89@hotmail.com>
2024-02-22 23:19:11 -0300

    changes

--- src/app/components/ButtonCTA/index.tsx
+++ src/app/components/ButtonCTA/index.tsx
@@ -1,9 +1,13 @@
-export default function ButtonCTA () {
+import Link from "next/link";
+
+export default function ButtonCTA() {
   return (
     <>
-      <button className='btn rounded-lg glass drop-shadow-[5px_5px_15px_rgba(227,80,14,0.8)] text-white bg-gradient-to-r from-red-700 via-orange-500 to-orange-600 font-bold ease-in-out delay-75 hover:transition-all hover:scale-105 duration-300 hover:text-white focus:outline-none focus:ring focus:ring-white active:text-opacity-75 hover:ring hover:ring-white hover:ring-offset-0 btn-md sm:btn-lg md:btn-lg lg:btn-lg'>
-        junte-se à lista de espera
+      <button className="btn rounded-lg glass drop-shadow-[5px_5px_15px_rgba(227,80,14,0.8)] text-white bg-gradient-to-r from-red-700 via-orange-500 to-orange-600 font-bold ease-in-out delay-75 hover:transition-all hover:scale-105 duration-300 hover:text-white focus:outline-none focus:ring focus:ring-white active:text-opacity-75 hover:ring hover:ring-white hover:ring-offset-0 btn-md sm:btn-lg md:btn-lg lg:btn-lg">
+        <Link href="https://www.asaas.com/c/jdhffxwrawrxhtlt" target="_blank">
+          junte-se à lista de espera
+        </Link>
       </button>
     </>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,44 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+const GTM_ID = "GTM-TBN2QTK5";
 
 export const metadata = {
-  title: 'Anderson Miranda',
-  description: 'Especialista e Professor no Mercado Financeiro na Comunidade TraderX',
-}
+  title: "Anderson Miranda",
+  description:
+    "Especialista e Professor no Mercado Financeiro na Comunidade TraderX",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className="antialiased">{children}</body>
+      {/* <meta
+        name="facebook-domain-verification"
+        content="0pp7qrk8a62xbewr980vkh2xrevbua"
+      /> */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <body className="antialiased">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
